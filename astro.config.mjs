8bd3eec468c7eb3259877dc644170fcdb957ebaf
Patryk Opala <patrykopala265@gamil.com>
2024-03-08 15:46:02 +0100

    v45

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    //site: 'https://patrykopala.github.io',
-    //base: '/dupa-portal'
+    site: 'https://patrykopala.github.io',
+    base: '/dupa-portal',
     outDir: './docs'
 });
